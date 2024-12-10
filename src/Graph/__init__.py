b18c7e6644718e6e08cf5fed972f27219ca76058
NTGNguyen <23521049@gm.uit.edu.vn>
2024-12-10 18:08:25 +0700

    chore!: add __init__ file for import from main.py

---
+++ src/Graph/__init__.py
@@ -0,0 +1,7 @@
+"""Modules"""
+from .edge import Edge
+from .frame_graph import FrameGraph
+from .node import Node
+from .window import CWindow
+
+__all__: list[str] = ["Edge","FrameGraph","Node","CWindow"]
---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+"""Modules"""
+from .Graph import Edge,FrameGraph,Node,CWindow
+from .utils import coordinate_random,coordinate_list_random
+
+__all__:list[str] = ["Edge","FrameGraph","Node","CWindow","coordinate_random","coordinate_list_random"]
