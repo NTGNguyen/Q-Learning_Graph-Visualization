9ebafeecdf6a6f674b2e813a54d3bfe0a4bfcb88
NTGNguyen <23521049@gm.uit.edu.vn>
2024-12-11 12:48:11 +0700

    chore(format): fix error format FLK-E501

--- src/__init__.py
+++ src/__init__.py
@@ -4,5 +4,5 @@ from .Q_Learning import GraphNetworkX, GraphNetworkXForQLearning
 from .utils import (DEFAULT_EDGES_NETWORKX, HEIGHT_WINDOW, WIDTH_WINDOW,
                     coordinate_list_random, coordinate_random)
 
-__all__: list[str] = ["Edge", "FrameGraph", "Node",
-                      "CWindow", "coordinate_random", "coordinate_list_random", "GraphNetworkX", "GraphNetworkXForQLearning", "WIDTH_WINDOW", "HEIGHT_WINDOW", "DEFAULT_EDGES_NETWORKX"]
+__all__: list[str] = ["Edge", "FrameGraph", "Node", "CWindow", "coordinate_random", "coordinate_list_random",
+                      "GraphNetworkX", "GraphNetworkXForQLearning", "WIDTH_WINDOW", "HEIGHT_WINDOW", "DEFAULT_EDGES_NETWORKX"]
