7daebb157fbe09c500b73324da3a250ba14b7b08
NTGNguyen <23521049@gm.uit.edu.vn>
2024-12-21 14:39:24 +0700

    feat: change color of nodes,edges on specific number

--- src/Graph/button.py
+++ src/Graph/button.py
@@ -16,11 +16,19 @@ class DrawRandomButton(ttk.Button):
     """The Button inherits from ttk.Button"""
 
     def __init__(self, parent: 'ButtonFrame', graph_frame: 'GraphFrame'):
+        """The button to draw graph
+
+        Args:
+            parent (ButtonFrame): The Frame contains the Button
+            graph_frame (GraphFrame): The Frame contains the graph
+        """
         super().__init__(parent, text="Draw Random Graph", command=self.draw_random_graph)
         self.pack(side=LEFT, padx=5, pady=5)
         self.graph_frame: GraphFrame = graph_frame
 
     def draw_random_graph(self):
+        """Function to draw random graph
+        """
         for widget in self.graph_frame.winfo_children():
             widget.destroy()
         self.Gr = GraphRandomGenerate()
@@ -29,7 +37,11 @@ class DrawRandomButton(ttk.Button):
                 edge_color=self.Gr.edge_colors, node_size=500, font_size=10)
         self.node_collection: plt.Collection = ax.collections[0]
         self.edge_collection: plt.Collection = ax.collections[1]
+        self.Gr.node_collection = ax.collections[0]
+        self.Gr.edge_collection = ax.collections[1]
         self.canvas = FigureCanvasTkAgg(fig, master=self.graph_frame)
+        self.Gr.canvas = self.canvas
         self.canvas_widget = self.canvas.get_tk_widget()
         self.canvas_widget.pack(fill=BOTH, expand=True)
         self.canvas.draw()
+        self.Gr.change_edge_color(1, 2)
--- src/Graph/graph.py
+++ src/Graph/graph.py
@@ -1,15 +1,21 @@
 """The Graph"""
 import random
+from typing import Any
 
 import networkx as nx
 from networkx import Graph
+import matplotlib.pyplot as plt
 
 
 class GraphRandomGenerate:
     """The Graph class contains their elements"""
 
     def __init__(self, default_node_color: str = 'skyblue', default_edge_color: str = 'black'):
-        """The initialization of GraphX class
+        """The Initialization of this class
+
+        Args:
+            default_node_color (str, optional): color of all the nodes in the first. Defaults to 'skyblue'.
+            default_edge_color (str, optional): color of all the edges in the first. Defaults to 'black'.
         """
         self.num_nodes: int = random.randint(10, 15)
         self.G: Graph = nx.gnp_random_graph(self.num_nodes, 0.4)
@@ -18,3 +24,32 @@ class GraphRandomGenerate:
             default_node_color for _ in range(len(self.G.nodes))]
         self.edge_colors: list[str] = [
             default_edge_color for _ in range(len(self.G.edges))]
+        self.node_collection: plt.Collection
+        self.edge_collection: plt.Collection
+        self.canvas: Any
+
+    def change_node_color(self, node_number: int) -> None:
+        """Change node color with specific number of node
+
+        Args:
+            node_number (int): The node number
+        """
+        if not self.G:
+            return
+        node_id = list(self.G.nodes)[node_number]
+        self.node_colors[node_number] = "red"
+        self.node_collection.set_facecolor(self.node_colors)
+        self.canvas.draw_idle()
+
+    def change_edge_color(self, first_node_number: int, second_node_number: int) -> None:
+        """Change edge color with specific number of two nodes in connection
+
+        Args:
+            first_node_number (int): number of first node in connection
+            second_node_number (int): number of second node in connection
+        """
+        edge_index = list(self.G.edges).index((first_node_number, second_node_number)if (
+            first_node_number, second_node_number) in self.G.edges else (second_node_number, first_node_number))
+        self.edge_colors[edge_index] = "red"
+        self.edge_collection.set_edgecolor(self.edge_colors)
+        self.canvas.draw_idle()
